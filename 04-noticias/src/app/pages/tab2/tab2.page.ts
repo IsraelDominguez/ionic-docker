94ab11b901ea13264e12736da7157b51578243ff
idominguez <israel.dominguez.garcia@gmail.com>
2019-12-13 12:04:05 +0100

    refres by category

--- 04-noticias/src/app/pages/tab2/tab2.page.ts
+++ 04-noticias/src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {IonSegment} from "@ionic/angular";
 import {NoticiasService} from "../../services/noticias.service";
+import {Article} from "../../interfaces/Article";
 
 @Component({
   selector: 'app-tab2',
@@ -12,13 +13,24 @@ export class Tab2Page implements OnInit{
   @ViewChild(IonSegment, {static:true}) segment: IonSegment;
 
   categories = ['business', 'entertainment','general','health','science','sports','technology'];
+  noticias: Article[] = [];
 
   constructor(private noticiasService: NoticiasService) {}
 
   ngOnInit(): void {
     this.segment.value = this.categories[0];
-    this.noticiasService.getTopHeadlinesByCategory(this.categories[0]).subscribe(resp => {
-      console.log(resp.articles);
+    this.loadNews();
+  }
+
+  refreshNews(event) {
+    this.noticias = [];
+    this.segment.value = event.detail.value;
+    this.loadNews()
+  }
+
+  loadNews() {
+    this.noticiasService.getTopHeadlinesByCategory(this.segment.value).subscribe(resp => {
+      this.noticias.push(...resp.articles);
     });
   }
 }
