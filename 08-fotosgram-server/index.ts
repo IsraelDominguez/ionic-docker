49b431213f3b66f4b93369919070e0106a5ce426
idominguez <israel.dominguez.garcia@gmail.com>
2019-12-17 17:04:55 +0100

    connecto to mongo

--- 08-fotosgram-server/index.ts
+++ 08-fotosgram-server/index.ts
@@ -1,10 +1,20 @@
 import Server from './classes/server';
 import userRoutes from "./routes/users";
+import mongoose from 'mongoose';
 
 const server = new Server();
 
 // App Routes
 server.app.use('/user', userRoutes);
+
+// Connect to Mongo with index
+mongoose.connect('mongodb://172.19.0.2:27017/fotosgram', {useNewUrlParser: true, useCreateIndex:true},
+    (err) => {
+        if (err) throw err;
+
+        console.log("Connected to DataBase")
+});
+
 
 // Up Express Server
 server.start( () => {
